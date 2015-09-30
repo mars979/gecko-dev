54e072f5c9ae806478eb33b41ba76b34438472e3
Georgiy Frolov <gfrolov@sensics.com>
2015-09-30 10:48:00 -0400

    add osvr pref and rename pref paths for OSVR to make it more consistent

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4644,16 +4644,18 @@ pref("dom.vr.oculus.enabled", true);
 pref("dom.vr.oculus050.enabled", true);
 // Cardboard VR device is disabled by default
 pref("dom.vr.cardboard.enabled", false);
+// OSVR device
+pref("dom.vr.osvr.enabled", true);
 // 0 = never; 1 = only if real devices aren't there; 2 = always
 pref("dom.vr.add-test-devices", 1);
 // true = show the VR textures in our compositing output; false = don't.
 // true might have performance impact
 pref("gfx.vr.mirror-textures", false);
 // path to OSVR DLLs
-pref("gfx.vr.osvrUtil.lib", "");
-pref("gfx.vr.osvrCommon.lib", "");
-pref("gfx.vr.osvrClient.lib", "");
-pref("gfx.vr.osvrClientKit.lib", "");
+pref("gfx.vr.osvr.utilLibPath", "");
+pref("gfx.vr.osvr.commonLibPath", "");
+pref("gfx.vr.osvr.clientLibPath", "");
+pref("gfx.vr.osvr.clientKitLibPath", "");
 
 // MMS UA Profile settings
 pref("wap.UAProf.url", "");
